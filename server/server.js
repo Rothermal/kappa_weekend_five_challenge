e08329910fe1024da36bdf7461f2b1762d2bcdbb
Mark Rothermal <rothermal2@comcast.net>
2016-03-28 09:47:00 -0500

    cleaned up

--- server/server.js
+++ server/server.js
@@ -11,22 +11,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static('server/public'));
 
-
-///////////////////////////////////////
-//var mongoURI = 'mongodb://localhost:27017/';
-//var mongoDB = mongoose.connect(mongoURI).connection;
-//
-//mongoDB.on('error', function(err){
-//    console.log('MongoDB error: ', err);
-//});
-//
-//mongoDB.on('open', function(){
-//    console.log('MongoDB connected!');
-//});
-///////////////////////////////////////
-
-
-
 app.use('/',index);
 
 var server = app.listen(port,function(){
